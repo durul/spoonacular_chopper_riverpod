373a687b75a8abd10804539d1054909927c1eb76
durul dalkanat <adurul17@hotmail.com>
2024-09-10 16:52:35 -0400

    Adding Streams to Bookmarks
    The Bookmarks page uses Consumer, but I changed it to a stream so it can react when a user bookmarks a recipe.

--- lib/ui/bookmarks/bookmarks.dart
+++ lib/ui/bookmarks/bookmarks.dart
@@ -17,6 +17,16 @@ class Bookmarks extends ConsumerStatefulWidget {
 
 class _BookmarkState extends ConsumerState<Bookmarks> {
   List<Recipe> recipes = [];
+  late Stream<List<Recipe>> recipeStream;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // This watches the repository for changes and updates the widget
+    final repository = ref.read(repositoryProvider.notifier);
+    recipeStream = repository.watchAllRecipes();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,95 +34,99 @@ class _BookmarkState extends ConsumerState<Bookmarks> {
   }
 
   Widget _buildBookmarks(BuildContext context) {
-    // This watches the repository for changes and updates the widget
-    final repository = ref.watch(repositoryProvider);
-    recipes = repository.currentRecipes;
-
     /// Delete Recipe
     void deleteRecipe(Recipe recipe) {
       ref.read(repositoryProvider.notifier).deleteRecipe(recipe);
     }
 
-    return SliverLayoutBuilder(
-      builder: (BuildContext context, SliverConstraints constraints) {
-        return SliverList.builder(
-          itemCount: recipes.length,
-          itemBuilder: (BuildContext context, int index) {
-            final recipe = recipes[index];
-            return SizedBox(
-              height: 100,
-              child: Slidable(
-                startActionPane: ActionPane(
-                  motion: const DrawerMotion(),
-                  extentRatio: 0.25,
-                  children: [
-                    SlidableAction(
-                      label: 'Delete',
-                      backgroundColor: Colors.transparent,
-                      foregroundColor: Colors.black,
-                      icon: Icons.delete,
-                      onPressed: (context) {
-                        deleteRecipe(recipe);
-                      },
-                    ),
-                  ],
-                ),
-                endActionPane: ActionPane(
-                  motion: const DrawerMotion(),
-                  extentRatio: 0.25,
-                  children: [
-                    SlidableAction(
-                      label: 'Delete',
-                      backgroundColor: Colors.transparent,
-                      foregroundColor: Colors.black,
-                      icon: Icons.delete,
-                      onPressed: (context) {
-                        deleteRecipe(recipe);
-                      },
-                    ),
-                  ],
-                ),
-                child: GestureDetector(
-                  onTap: () {
-                    /// Push to Recipe Details Page
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => RecipeDetails(
-                          recipe: recipe.copyWith(bookmarked: true),
+    return StreamBuilder<List<Recipe>>(
+      stream: recipeStream,
+      builder: (context, AsyncSnapshot<List<Recipe>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          recipes = snapshot.data ?? [];
+        }
+        return SliverLayoutBuilder(
+          builder: (BuildContext context, SliverConstraints constraints) {
+            return SliverList.builder(
+              itemCount: recipes.length,
+              itemBuilder: (BuildContext context, int index) {
+                final recipe = recipes[index];
+                return SizedBox(
+                  height: 100,
+                  child: Slidable(
+                    startActionPane: ActionPane(
+                      motion: const DrawerMotion(),
+                      extentRatio: 0.25,
+                      children: [
+                        SlidableAction(
+                          label: 'Delete',
+                          backgroundColor: Colors.transparent,
+                          foregroundColor: Colors.black,
+                          icon: Icons.delete,
+                          onPressed: (context) {
+                            deleteRecipe(recipe);
+                          },
                         ),
-                      ),
-                    );
-                  },
-                  child: Card(
-                    elevation: 1.0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
+                      ],
                     ),
-                    color: Colors.white,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          leading: CachedNetworkImage(
-                            imageUrl: recipe.image ?? '',
-                            height: 120,
-                            width: 60,
-                            fit: BoxFit.cover,
+                    endActionPane: ActionPane(
+                      motion: const DrawerMotion(),
+                      extentRatio: 0.25,
+                      children: [
+                        SlidableAction(
+                          label: 'Delete',
+                          backgroundColor: Colors.transparent,
+                          foregroundColor: Colors.black,
+                          icon: Icons.delete,
+                          onPressed: (context) {
+                            deleteRecipe(recipe);
+                          },
+                        ),
+                      ],
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        /// Push to Recipe Details Page
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => RecipeDetails(
+                              recipe: recipe.copyWith(bookmarked: true),
+                            ),
                           ),
-                          title: Text(recipe.label ?? ''),
+                        );
+                      },
+                      child: Card(
+                        elevation: 1.0,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        color: Colors.white,
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ListTile(
+                              leading: CachedNetworkImage(
+                                imageUrl: recipe.image ?? '',
+                                height: 120,
+                                width: 60,
+                                fit: BoxFit.cover,
+                              ),
+                              title: Text(recipe.label ?? ''),
+                            ),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
+                );
+              },
             );
           },
         );
+        // TODO: Add else here
       },
     );
-    // TODO: Add else here
   }
 }
