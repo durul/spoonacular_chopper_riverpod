97fa392609b3102e137cff539b641907061c8fcd
durul dalkanat <adurul17@hotmail.com>
2024-09-13 19:39:25 -0400

    Added additional test cases

--- test/ui/widgets/ingredient_card_test.dart
+++ test/ui/widgets/ingredient_card_test.dart
@@ -55,5 +55,78 @@ void main() {
       expect(checkboxFinder, findsOneWidget);
       expect(isChecked, isTrue);
     });
+
+    testWidgets('can be unchecked when tapped', (tester) async {
+      var isChecked = true;
+      await tester.pumpWidget(
+        buildWrappedWidget(IngredientCard(
+          name: mockIngredientName,
+          initiallyChecked: isChecked,
+          evenRow: true,
+          onChecked: (newValue) {
+            isChecked = newValue;
+          },
+        )),
+      );
+
+      // Act
+      final cardFinder = find.byType(IngredientCard);
+
+      await tester.tap(cardFinder);
+      await tester.pumpAndSettle();
+
+      final checkboxFinder = find.byType(Checkbox);
+
+      // Assert
+      expect(checkboxFinder, findsOneWidget);
+      expect(isChecked, isFalse);
+    });
+
+    // Add a test to verify that the evenRow parameter is respected.
+    testWidgets('can be styled differently based on evenRow parameter',
+        (tester) async {
+      // Arrange
+      await tester.pumpWidget(
+        buildWrappedWidget(IngredientCard(
+          name: mockIngredientName,
+          initiallyChecked: false,
+          evenRow: true,
+          onChecked: (isChecked) {},
+        )),
+      );
+
+      // Act
+      final cardFinder = find.byType(IngredientCard);
+      final card = tester.widget<IngredientCard>(cardFinder);
+
+      // Assert
+      expect(card.evenRow, isTrue);
+    });
+
+    // Add a test to verify that the onChecked callback is called when the checkbox is tapped.
+    testWidgets('can call onChecked callback when checkbox is tapped',
+        (tester) async {
+      // Arrange
+      var isChecked = false;
+      await tester.pumpWidget(
+        buildWrappedWidget(IngredientCard(
+          name: mockIngredientName,
+          initiallyChecked: isChecked,
+          evenRow: true,
+          onChecked: (newValue) {
+            isChecked = newValue;
+          },
+        )),
+      );
+
+      // Act
+      final checkboxFinder = find.byType(Checkbox);
+
+      await tester.tap(checkboxFinder);
+      await tester.pumpAndSettle();
+
+      // Assert
+      expect(isChecked, isTrue);
+    });
   });
 }
