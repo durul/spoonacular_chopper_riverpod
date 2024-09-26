ef05fb60a657b38aff44e340e9f336e4d6ff6d53
durul dalkanat <adurul17@hotmail.com>
2024-09-26 12:09:16 -0400

    [Flutter App Lifecycle: Using AppLifecycleListener](https://medium.com/flutter-minds/2024-best-practices-for-flutter-app-lifecycle-using-applifecyclelistener-3aa1dfd8ec6c)

--- lib/application.dart
+++ lib/application.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'providers.dart';
 import 'ui/main_screen.dart';
 import 'ui/theme/theme.dart';
+import 'utils/logger.dart';
 
 class Application extends ConsumerStatefulWidget {
   const Application({super.key});
@@ -15,6 +17,8 @@ class Application extends ConsumerStatefulWidget {
 
 class _MyAppState extends ConsumerState<Application> {
   ThemeMode currentMode = ThemeMode.light;
+  late final AppLifecycleListener _lifecycleListener;
+  late AppLifecycleState? _currentState;
 
   // MaterialColor? appConfigColor(AppConfig appConfig) {
   //   switch (appConfig.flavor) {
@@ -31,8 +35,46 @@ class _MyAppState extends ConsumerState<Application> {
   // }
 
   @override
+  void initState() {
+    super.initState();
+    _currentState = SchedulerBinding.instance.lifecycleState;
+
+    _lifecycleListener = AppLifecycleListener(
+      onShow: () => _handleState('show'),
+      onResume: () => _handleState('resume'),
+      onHide: () => _handleState('hide'),
+      onInactive: () => _handleState('inactive'),
+      onPause: () => _handleState('pause'),
+      onDetach: () => _handleState('detach'),
+      onRestart: () => _handleState('restart'),
+      onStateChange: _handleStateChange,
+    );
+  }
+
+  void _handleState(String state) {
+    setState(() {
+      logInfo('Current state: $state');
+    });
+  }
+
+  void _handleStateChange(AppLifecycleState state) {
+    setState(() {
+      _currentState = state;
+      logInfo('State changed to: $state');
+    });
+  }
+
+  @override
+  void dispose() {
+    _lifecycleListener.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     final appConfig = ref.watch(appConfigProvider);
+    // Add this line to use _currentState
+    logInfo('Current app lifecycle state: $_currentState');
 
     return PlatformMenuBar(
       menus: [
